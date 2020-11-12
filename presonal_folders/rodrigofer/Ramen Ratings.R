f9544e67e9a0f6b630dff387c8602f4e82e3a8e1
rodrigomaceno <fer18014@byui.edu>
2020-11-11 19:07:29 -0800

    w

--- presonal_folders/rodrigofer/Ramen Ratings.R
+++ presonal_folders/rodrigofer/Ramen Ratings.R
@@ -15,8 +15,15 @@ View(ramen)
 colnames(ramen) ## Gives column names
 
 USA_Ramen_Brands <- ramen %>%
-  filter(Country == "USA") %>% ## Lets us select down on rows
+  filter(Country == "USA", Stars == "5") %>% ## Lets us select down on rows
   select(Brand, Stars, Variety) %>% ##  This lets you narrow down columns / what column we want to look down on
-  unique()
+  #unique() 
+  #filter(Stars == "5")
+  group_by(Brand) %>%
+  summarise(count = n())
+colnames(USA_Ramen_Brands)
 
+USA_Ramen_Brands %>%
+  ggplot(aes(x = Brand, y = count, fill = Brand)) +
+  geom_col()
 View(USA_Ramen_Brands)  
